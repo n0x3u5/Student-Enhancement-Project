86438e560e6bd088885f9bcd62d8f1af1720baa7
Nakshatra Mukhopadhyay <nakshatra.mukhopadhyay1994@gmail.com>
2015-12-25 20:16:45 +0530

    Initial Commit
    Stuff done -
    [x] Creating a text area
    [x] Creating a robust timer
    [] Changing the background of the essay page
    [] Designing the Hoome Page

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Student Enhancement Project | Home</title>
+    <link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.6/material.indigo-pink.min.css">
+    <script src="https://storage.googleapis.com/code.getmdl.io/1.0.6/material.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  </head>
+  <body>
+    <h1>Let's get started.</h1>
+    <h2>What would you like to practice?</h2>
+    <ul>
+      <li><a href="essay_practice.php">Essay</a></li>
+    </ul>
+  </body>
+</html>
